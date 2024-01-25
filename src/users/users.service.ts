1bf5458d1f6b32cd28a8622f729a284b92da1932
anthondevg <robwert7991@gmail.com>
2024-01-25 12:55:06 -0400

    hashed password record

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,14 +3,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma.service';
 import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
+  // hash password
   async create(data: Prisma.UserCreateInput): Promise<User> {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
     return this.prisma.user.create({
-      data,
+      data: {
+        email: data.email,
+        password: hashedPassword,
+        name: data.name,
+      },
     });
   }
 
